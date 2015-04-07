5ca26635d4129a4ae931f605e1bd609f99df4142
Siequnu <patrickjohansson@root.local>
2015-04-07 23:19:31 +0100

    Relative chord notation, harmonic cadence and progression generation
    Chord chain now in relative notation. Script analyses phrasing to
    insert cadences and chord progressions.

--- chordCatalog.class.php
+++ chordCatalog.class.php
@@ -6,8 +6,38 @@ class chordCatalog {
      */
     public function __construct()
     {
-        # Creates a chord matrix
-        $chordMatrix = array(
+        # Creates a cypher progression matrix
+        $cypherMatrix = array(
+            'I' => array('V', 'vi', 'ii', 'iii'),
+            'ii' => array('iii', 'V', 'V', 'vi','I'),
+            'iii' => array('I', 'V', 'IV'),
+            'IV' => array ('V', 'I', 'bVII', 'vi'),
+            'V' => array( 'I', 'ii', 'iii'),
+            'vi' => array('ii', 'viidim', 'IV'),
+            'bVII' => 'IV',
+            'viidim' => 'iii', 
+        );
+        $this->cypherMatrix = $cypherMatrix;
+        
+        # Creates a 12 tone cypher scale
+        $cypherScale = array(
+            '1' => 'I',
+            '2' => 'bii',
+            '3' => 'ii',
+            '4' => 'biii',
+            '5' => 'iii',
+            '6' => 'IV',
+            '7' => 'bV',
+            '8' => 'V',
+            '9' => 'bvi',
+            '10' => 'vi',
+            '11' => 'bVII',
+            '12' => 'viidim',
+        );
+        $this->cypherScale = $cypherScale;
+        
+        # Creates a chord progression matrix
+        $fullChordMatrix = array(
             'c' => array('g6', 'am', 'dmaj7', 'dm'),
             'dm' => array('emaj7', 'g', 'g6', 'amaj7'),
             'dmaj7' => array('g', 'g6'),
@@ -19,16 +49,32 @@ class chordCatalog {
             'am' => array('dm', 'bmaj7'),
             'amaj7' => array('dm', 'bb'),
             'bb' => 'g6',
-            'bmaj7' => 'emaj7',
-            
+            'bmaj7' => 'emaj7', 
         );
+        $this->fullChordMatrix = $fullChordMatrix;
         
-        $this->chordMatrix = $chordMatrix;
+        # Creates a 12 tone scale array
+        $serialScale = array(
+            '1' => 'c',
+            '2' => 'c#',
+            '3' => 'd',
+            '4' => 'd#',
+            '5' => 'e',
+            '6' => 'f',
+            '7' => 'f#',
+            '8' => 'g',
+            '9' => 'g#',
+            '10' => 'a',
+            '11' => 'a#',
+            '12' => 'b',
+        );
+        $this->serialScale = $serialScale;
+        
     }
     
     
     /*
-     * Gets a matching chord from the matrix from a supplied matrix
+     * Gets a matching chord from the progression matrix from a supplied chord
      *
      * @param string $stemChord Chord which will trigger another chord
      *
@@ -37,7 +83,7 @@ class chordCatalog {
     public function getNextChord($stemChord){
         
         # Gets corresponding entry for $stemChord
-        $matchingChord = $this->chordMatrix[$stemChord];
+        $matchingChord = $this->cypherMatrix[$stemChord];
         
         # Tests if $matchingChord is itself an array and if TRUE
         # retrieves random entry from its array   
@@ -56,16 +102,14 @@ class chordCatalog {
      * @return string $randomMatchingChord Random chord from array
      */
     public function getRandomChordFromArray($array){
-        # Fetches the key of the last entry in array
         end($array);
         $lastIndexKey = key($array);
-        # Chooses random index
         $randomMatchingChord = rand(0,$lastIndexKey);
-        # Dives into the matrix and retrieves the randomly picked entry
         $matchingChord = $array[$randomMatchingChord];
         return $matchingChord;
     }
  
-    
+     
 }
+
 ?>
--- chordGenerator.class.php
+++ chordGenerator.class.php
@@ -2,91 +2,78 @@
 
 class chordGenerator {
     
-    # Class defaults
-    private $initialChord = 'c';
-    private $chordsPerBlock = 16;
-
+     # Class defaults
+     private $initialChord = 'C';
+     private $chordsPerBlock = 32;
     
-    public function __construct()
-    {
-        # Do nothing
-    }
+    
+     public function __construct()
+     {
+          # Do nothing
+     }
     
     
     /*
-     * Generates the form
+     * Generates a 2 part form
+     *
+     * @param str $prompt1 Message prompt
+     * @param str $prompt2 Second Prompt
+     * @param str $name1 Name of data entered
+     * @param str $name2 Second Name
+     * @param str $submitValue Message on submit button
      *
      * @return string
      */
-    private function generateForm () {
-         $formhtml='<html>
-    
-    
-            <head></head>
-            <body>
+     private function generateForm ($prompt1, $prompt2, $name1, $name2, $submitValue) {
+          $formhtml='<html>
+          <head></head>
+          <body>
             
-            <form action="' . $_SERVER['REQUEST_URI'] . '" method="post">
+          <form action="' . $_SERVER['REQUEST_URI'] . '" method="post">
             
-            How many verses: <input type="text" name="msg" size="30">
+          ' . $prompt1 . ': <input type="text" name=' . $name1 . ' size="10">
+          ' . $prompt2 . ': <input type="text" name=' . $name2 . ' size="10">
             
-            <input type="submit" value="Magically Generate Harmony!">
+          <input type="submit" value="' . $submitValue . '">
             
-            </form>
+          </form>
+          
+          </body>
             
-            </body>
-            
-            </html>';
-            return $formhtml;
-    }
-    
-    
-    /*
-     * Changes the initial chord
-     *
-     * @param string $initialChord Sets the initial chord
-     */
-    public function setInitialChord($initialChord)
-    {
-        $this->initialChord = $initialChord;
-    }
+          </html>';
+          return $formhtml;
+     }
     
     
     /*
      * Main entry into program 
      */
-    public function main()
-    {
-        # Generate form if not submitted
-        if (!isSet ($_POST['msg'])) {
-            $html=$this->generateForm();
-            echo $html;
-            return;
-        }
+     public function main()
+     {
+          # Generate form if not submitted
+          if (!isSet ($_POST['msg'])) {
+               $html=$this->generateForm('How many verses', 'Which starting chord?', 'msg', 'initialchord', 'Magically Generate Harmony!');
+               echo $html;
+               return;
+          }
         
-        # Retrieve form data and assign it
-        $numberOfBlocks = $_POST['msg'];
+          # Retrieve form data and assign it
+          $numberOfBlocks = $_POST['msg'];
+          $this->initialChord = $_POST['initialchord'];
+          
+          # Calculate total number of chords needed
+          $totalchords = $numberOfBlocks * $this->chordsPerBlock;
+          
+          # Generate sequence of chords
+          $sequence = $this->generateChords ($totalchords, $this->initialChord);
         
-        # Calculate total number of chords needed
-        $totalchords = $numberOfBlocks * $this->chordsPerBlock;
-        
-        # Check if initial chord is to be set otherwise set to default 'c'
-        if (isSet ($_GET["initialchord"])) {
-            $this->setInitialChord ($_GET["initialchord"]);
-        }
-        else {
-            $this->setInitialChord ('c');
-        }
-        
-        # Generate sequence of chords
-        $sequence = $this->generateChords ($totalchords, $this->initialChord);
-        
-        # Organize array into blocks and produce HTML
-        $html = $this->organizeSequence ($sequence,$numberOfBlocks);
-        echo $html;
-    }
+          # Organize array into blocks and produce HTML
+          $html = $this->organizeSequence ($sequence,$numberOfBlocks);
+          echo $html;
+     }
     
     
-    /*
+     /*
      * Loop that generates chords starting with a given initial chord
      * 
      * @param int $totalchords Positive integer representing wanted number of chords
@@ -94,27 +81,123 @@ class chordGenerator {
      * 
      * @return string String of logically generated chords 
      */
-    private function generateChords ($totalChords, $firstChord)
-    {
-        # Initialise chordCatalog.php
-        include 'chordCatalog.class.php';
-        $chordCatalog1 = new chordCatalog;
+     public function generateChords ($totalChords, $firstChord)
+     {
+          include 'chordCatalog.class.php';
+          $chordCatalog = new chordCatalog;
+          $this->chordCatalog = $chordCatalog;
         
-        # Initialise an array of chords
-        $this->sequence = array ();
-
-        # Set the first item to be the first chord
-        $this->sequence[] = $firstChord;
-        
-        for ($generationCycles = 1; $generationCycles <= $totalChords; $generationCycles++)
-        {   
-            # Generate new chord based on existing array
-            # Write the newly generated chord in to the array
-            $this->sequence[] = $chordCatalog1->getNextChord(end($this->sequence));
-        }
-        
-        return $this->sequence;
-    }
+          # Convert first chord into cipher
+          //DEV system to convert maj min chord into cypher
+          $startingCypher = $firstChord;
+          $startingCypher = 'I';
+          
+          # Initialise and populate an array of cyphers
+          $this->sequence = array ();
+          $this->sequence[0] = $startingCypher;
+          for ($generationCycles = 1; $generationCycles < $totalChords; $generationCycles++)
+          {   
+               # Generate new chord based on existing array and write to new index
+               $this->sequence[] = $chordCatalog->getNextChord(end($this->sequence));
+          }
+          # Set last chord same as first
+          $this->sequence[($this->chordsPerBlock-1)] = $this->sequence[0];
+          
+          # Populate sequences and cadences from given indices
+          
+          #$neededCadences = array ('0', ($this->chordsPerBlock-1) )
+          
+          $index = '0';
+          $this->cadenceGenerator ($index);
+ 
+          # DEV Convert sequence of cyphers into chords
+              
+          return $this->sequence;
+     }
+     
+     
+     /*
+      * Inserts a generated cadence into the sequence from a given index
+      *
+      * @param string $initialIndex Index of cypher, provides root and position
+      */    
+     public function cadenceGenerator ($initialIndex) {
+          $initialCypher = $this->sequence[$initialIndex];
+          
+          # Pick random cadence style and generate array
+          $randomChoice = mt_rand (1,3);
+          $resultArray = $this->cadenceArrayGenerator ($initialCypher, $randomChoice);
+          
+          # Merge result with existing sequence starting after given index
+          foreach ($resultArray as $generatedCypher) {
+               $cypherIndex = array_search($generatedCypher, $resultArray);
+               $this->sequence[($initialIndex+1+$cypherIndex)] = $generatedCypher;
+          }
+     }
+     
+    
+     /*
+      * Generates a 3 chord cadence from a given cypher
+      *
+      * @param string $initialCypher The root cypher of cadence
+      * @param int $choiceIndicator Indicates which cadence to generate
+      *
+      * @return array $resultArray Array with cyphers in [0], [1]; [2] = root chord cypher
+      */
+     public function cadenceArrayGenerator ($initialCypher, $choiceIndicator) {
+          # Calculate the index of root cypher, returns a string
+          $chordIndex = array_search ($initialCypher, $this->chordCatalog->cypherScale); 
+          # Calculate position of cadence chords
+          switch ($choiceIndicator) {
+               case 1:
+                    $firstCypherOffset = 2;
+                    $secondCypherOffset = 5;
+                    break;
+               case 2:
+                    $firstCypherOffset = 5;
+                    $secondCypherOffset = 2;
+                    break;
+               case 3:
+                    $firstCypherOffset = 5;
+                    $secondCypherOffset = 6;
+                    break;
+          }     
+          $firstCypherIndex = $this->serialIndexCorrector (($chordIndex + $firstCypherOffset));
+          $secondCypherIndex = $this->serialIndexCorrector (($firstCypherIndex + $secondCypherOffset));
+          # Convert index positions into cyphers and place in array   
+          $resultArray = array ($this->chordCatalog->cypherScale[$firstCypherIndex], $this->chordCatalog->cypherScale[$secondCypherIndex], $initialCypher);
+          return $resultArray;
+     }
+     
+     
+     /*
+      * Gets serial index of a note
+      *
+      * @param string $note The note you want to check
+      *
+      * @return int The serial index of the note
+      */
+     public function getNoteSerialIndex ($note) {
+          $noteIndex = array_search ($note, $this->chordCatalog->serialScale);
+          return $noteIndex;
+     }
+     
+    /*
+     * Makes sure serial index is between 1 and 12
+     * 
+     * @param int $serialIndex The index to be checked
+     *
+     *@return int The equivalent index between 1-12
+     */
+     public function serialIndexCorrector ($serialIndex) {
+          if ($serialIndex <= 0) {
+               $serialIndex = ($serialIndex + 12);
+          }
+          if ($serialIndex > 12) {
+               $serialIndex = ($serialIndex - 12);
+          }
+          return $serialIndex;
+     }
     
     
     /* 
@@ -125,39 +208,34 @@ class chordGenerator {
      *
      * @return string $html HTML of formatted chords
      */ 
-    private function organizeSequence($sequence,$numberOfBlocks)
-    {
-        # Start the HTML
-        $html="\n";
+     private function organizeSequence($sequence,$numberOfBlocks)
+     {
+          # Start the HTML
+          $html='<head></head>
+          <body>';
+         
+          $numberOfLines = ($this->chordsPerBlock/8);
+          $numberOfColumns = ($this->chordsPerBlock/4);
         
-        $numberOfLines = sqrt($this->chordsPerBlock);
-        $numberOfColumns = sqrt($this->chordsPerBlock);
-        
-        # Organize chords into pattern
-        
-        $arrayPosition=0;
-        for($block=1; $block<=$numberOfBlocks; $block++) {
-                
-            for($line=1; $line<=$numberOfLines; $line++) {
-            
-                for ($column = 1; $column <= $numberOfColumns; $column++)
-                {
-                    $html .= $sequence[$arrayPosition] . ', ';
-                
-                    $arrayPosition++;    
-                }
-                
-                $html.="<br />\n";
-            }
-            
-            if  ($block != $numberOfBlocks){
-                $html .= "<br />\n";
-            }
-        }
-        
-        return $html;   
+          # Process array into blocks, lines, columns
+          $arrayPosition=0;
+          for($block=1; $block<=$numberOfBlocks; $block++) {
+               for($line=1; $line<=$numberOfLines; $line++) {  
+                    for ($column = 1; $column <= $numberOfColumns; $column++)
+                    {
+                         $html .= $sequence[$arrayPosition] . ', ';
+                         $arrayPosition++;    
+                    } 
+                    $html.="<br />\n";
+               }
+               if  ($block != $numberOfBlocks){
+               $html .= "<br />\n";
+               }
+          }
+          return $html;   
     }
 
+    
 }
 
 ?>
